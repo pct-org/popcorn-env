f2276b19c76518f5c78a3a226830ed64b5f2d935
Tycho Bokdam <tycho@palmtreecoding.com>
2020-10-23 14:43:53 +0200

    refactor(trakt): Updated season interface

--- libs/services/trakt/src/interfaces/trakt.season.interface.ts
+++ libs/services/trakt/src/interfaces/trakt.season.interface.ts
@@ -1,5 +1,35 @@
+import { TraktEpisode } from './trakt.episode.interface'
+
 export interface TraktSeason {
 
+  ids: {
+    trakt: number
+
+    tvdb: number
+
+    tmdb: number
+
+    // tvrage: object
+  }
+
   number: number
+
+  rating: number
+
+  votes: number
+
+  episode_count: number
+
+  aired_episodes: number
+
+  title: string
+
+  overview: string
+
+  first_aired: string
+
+  network: string
+
+  episodes: TraktEpisode[]
 
 }
